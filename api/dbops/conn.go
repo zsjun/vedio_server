c67c00420ad22ff5b9eb4f86201808b13ff9121a
zsj <zoushijun@threatbook.cn>
2020-04-05 11:00:25 +0800

    fix

---
+++ api/dbops/conn.go
@@ -0,0 +1,18 @@
+package dbops
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	dbConn *sql.DB
+	err error
+)
+
+func init() {
+	dbConn, err = sql.Open("mysql", "root:zsjzsj123@tcp(localhost:3306)/videoServer?charset=utf8")
+	if err != nil {
+		panic(err.Error())
+	}
+}
